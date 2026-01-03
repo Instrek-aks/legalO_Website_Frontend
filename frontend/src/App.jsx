bdf603dd51d96cfd2477c11d8d84d49e95ce051d
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2026-01-03 18:45:18 +0530

    pop up remove

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -43,45 +43,45 @@ const App = () => {
   }, []);
 
   return (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <ScrollToTop />
-        <FloatingRegisterButton />
-          <WelcomePopup
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <ScrollToTop />
+          <FloatingRegisterButton />
+          {/* <WelcomePopup
             isOpen={isPopupOpen}
             onClose={() => setIsPopupOpen(false)}
-          />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/podcasts" element={<Podcasts />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/pages" element={<Pages />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-of-service" element={<TermsOfService />} />
+          /> */}
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/podcasts" element={<Podcasts />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/pages" element={<Pages />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
 
-          {/* LegalO Routes */}
-          {/* <Route path="/legal-o/olympiad" element={<Olympiad />} />
+            {/* LegalO Routes */}
+            {/* <Route path="/legal-o/olympiad" element={<Olympiad />} />
           <Route path="/legal-o/ai-tools" element={<AITools />} />
           <Route path="/legal-o/podcasts" element={<Podcasts />} />
           <Route path="/legal-o/find-mentor" element={<FindMentor />} />
           <Route path="/legal-o/subjects" element={<Subjects />} />
           <Route path="/legal-o/mentoring" element={<Mentoring />} /> */}
 
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
